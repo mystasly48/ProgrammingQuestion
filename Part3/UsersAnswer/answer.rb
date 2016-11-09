209aa8947693d40c562d9cbe6da77143b989501a
iYaipH <irimamekun@gmail.com>
2016-11-10 00:43:33 +0900

    Update answer.rb

--- Part3/UsersAnswer/answer.rb
+++ Part3/UsersAnswer/answer.rb
@@ -1,1 +1,1 @@
- puts ((n, m = gets, gets.chomp.split(' ').map(&:to_i))[1].reduce{ |sum, n| n + (sum *= 1.08)}).round(0)
+puts ((n, m = gets, gets.chomp.split(' ').map(&:to_i))[1].map{|v| v *= 1.08 }.reduce(:+)).round(0)
