0f52c577d8c12f0051015e0dbc952831c430f9e0
KitunEudon <r.toku357951@gmail.com>
2016-11-09 19:59:54 +0900

    Part1 Answer
    パート1の解答です

---
+++ Part1/Answer01.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Answer01
+{
+    public static void Main()
+    {
+        Console.WriteLine(Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray().Sum());
+    }
+}
