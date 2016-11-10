56173af4769246f9a0aa21654833821d9285c73e
bakeKitunE <r.toku357951@gmail.com>
2016-11-10 16:38:56 +0900

    Part3 Answer
    パート３の回答です。

---
+++ Part3/UsersAnswer/KitunEudon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Answer
+{
+    public class Answer
+    {
+        public static void Main()
+        {
+            // 入力された値の消費税込みの値を出力
+            Console.WriteLine(Console.ReadLine().Split(' ').Select(int.Parse).ToArray().Sum() * 1.08);
+        }
+    }
+}
