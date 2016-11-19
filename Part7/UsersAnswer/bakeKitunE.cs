13ed7fb867e493b5b28ecf660919c0af97d107a3
KitunEudon <r.toku357951@gmail.com>
2016-11-19 22:22:25 +0900

    [Answer] Part7
    パート７の解答

---
+++ Part7/UsersAnswer/bakeKitunE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Console.ReadLine();
+        }
+        if (Console.ReadLine() == "ドコ")
+        {
+            Console.WriteLine("キヨシ！");
+        }
+        else
+        {
+            Console.WriteLine("ズコー！");
+        }
+    }
+}
