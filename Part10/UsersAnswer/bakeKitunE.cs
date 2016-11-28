d4042d1adadf16c642356faf9d8b89c9e2ab327b
bakeKitunE <r.toku357951@gmail.com>
2016-11-28 15:01:19 +0900

    変更　double => int

--- Part10/UsersAnswer/bakeKitunE.cs
+++ Part10/UsersAnswer/bakeKitunE.cs
@@ -13,6 +13,6 @@ class Program
         DateTime afterTime = new DateTime(after[0], after[1], after[2], after[3], after[4], after[5]);
 
         TimeSpan time = afterTime - nowTime;
-        Console.WriteLine(time.TotalHours);
+        Console.WriteLine((int)time.TotalHours);
     }
 }
