2073740d28c9d47f8e74febeccadf819c46a4b29
iYaipH <irimamekun@gmail.com>
2016-12-12 13:35:42 +0900

    Comvert indent Golang

--- Part2/UsersAnswer/JpnLavender.go
+++ Part2/UsersAnswer/JpnLavender.go
@@ -1,38 +1,38 @@
 package main
 
-import(
-    "fmt"
-    "os"
-    "bufio"
-    "strconv"
-    "math/rand"
-    "time"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
 var input = bufio.NewScanner(os.Stdin)
 
 func gets() string {
-    if input.Scan() {
-        return input.Text()
-    } else {
-        return "NotInputData"
-    }
+	if input.Scan() {
+		return input.Text()
+	} else {
+		return "NotInputData"
+	}
 }
 
 func parseint(getValue string) int {
-    var parseResult int
-    parseResult, _ = strconv.Atoi(getValue)
-    return parseResult
+	var parseResult int
+	parseResult, _ = strconv.Atoi(getValue)
+	return parseResult
 }
 
 func createRandomValue(randomRange int) int {
-    rand.Seed(time.Now().UnixNano())
-    return rand.Intn(randomRange)
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(randomRange)
 }
 
 func main() {
-    getValue := gets()
-     randomRange := parseint(getValue)
-    answer := createRandomValue(randomRange)
-    fmt.Println(answer)
+	getValue := gets()
+	randomRange := parseint(getValue)
+	answer := createRandomValue(randomRange)
+	fmt.Println(answer)
 }
--- Part5/UsersAnswer/JpnLavender.go
+++ Part5/UsersAnswer/JpnLavender.go
@@ -1,66 +1,70 @@
 package main
 
-import(
-    "fmt"
-    "os"
-    "bufio"
-    "time"
-    "strconv"
-    "strings"
-    "math/rand"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var input = bufio.NewScanner(os.Stdin)
 
 func gets() int {
-    if input.Scan() {
-        var result int
-        result, _ = strconv.Atoi(input.Text())
-        return result
-    }else {
-        return 404
-    }
+	if input.Scan() {
+		var result int
+		result, _ = strconv.Atoi(input.Text())
+		return result
+	} else {
+		return 404
+	}
 }
 
 func createAlphabetList() []string {
-    alphabets  := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-    return alphabets
+	alphabets := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	return alphabets
 }
 
-func createAnswrValue(countNum int , alphabets []string) []string {
-    var result []string
-    for {
-        newAlphabet := sample(alphabets)
-        if include(result, newAlphabet) {
-            continue
-        } else {
-            result = append(result, newAlphabet)
-            if len(result) >= countNum {break}
-        }
-    }
-    return result
+func createAnswrValue(countNum int, alphabets []string) []string {
+	var result []string
+	for {
+		newAlphabet := sample(alphabets)
+		if include(result, newAlphabet) {
+			continue
+		} else {
+			result = append(result, newAlphabet)
+			if len(result) >= countNum {
+				break
+			}
+		}
+	}
+	return result
 }
 
-func changer (answerValue []string) string {
-    return strings.Join(answerValue, " ")
+func changer(answerValue []string) string {
+	return strings.Join(answerValue, " ")
 }
 
 func sample(s []string) string {
-    rand.Seed(time.Now().UnixNano())
-    i := rand.Intn(len(s))
-    return s[i]
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(s))
+	return s[i]
 }
 
-func include( ary []string, findValue string ) bool {
-    for _, value := range ary {
-        if value == findValue {return true}
-    }
-    return false
+func include(ary []string, findValue string) bool {
+	for _, value := range ary {
+		if value == findValue {
+			return true
+		}
+	}
+	return false
 }
 
-func main(){
-    var dick int = gets()
-    answerValue := createAnswrValue(dick, createAlphabetList())
-    answer := changer(answerValue)
-    fmt.Println(answer)
+func main() {
+	var dick int = gets()
+	answerValue := createAnswrValue(dick, createAlphabetList())
+	answer := changer(answerValue)
+	fmt.Println(answer)
 }
