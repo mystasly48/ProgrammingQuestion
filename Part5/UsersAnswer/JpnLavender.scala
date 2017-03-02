ff22cbf73e2824a6476ebe301b5483d53050943f
JpnLavender <irimamekun@gmail.com>
2017-03-02 15:59:26 +0900

    Part5にScalaで回答を追加

---
+++ Part5/UsersAnswer/JpnLavender.scala
@@ -0,0 +1,11 @@
+import scala.util.Random
+
+object Main extends App{
+  var in = readLine().toInt
+  var alfa = List("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+  var random = new Random()
+  var parsedAlfa = Random.shuffle(alfa)
+  for ( i <- 0 to in ) {
+    print(parsedAlfa(i))
+  }
+}
