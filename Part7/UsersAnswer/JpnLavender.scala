c43b274fd72f3cde9fdddcf648e4e4177258b479
JpnLavender <irimamekun@gmail.com>
2017-03-02 16:30:15 +0900

    Part7にScalaで回答を追加

---
+++ Part7/UsersAnswer/JpnLavender.scala
@@ -0,0 +1,12 @@
+object Main extends App{
+  var s1 = readLine()
+  var s2 = readLine()
+  var s3 = readLine()
+  var s4 = readLine()
+  var main = readLine()
+  if (main == "ドコ") {
+    println("キヨシ！")
+  } else {
+    println("ズコー！")
+  }
+}
