6435c60e7739c4efdb78b1054e1b5b8876cebbd5
JpnLavender <irimamekun@gmail.com>
2017-03-02 18:20:47 +0900

    Part5の修正

--- Part5/UsersAnswer/JpnLavender.scala
+++ Part5/UsersAnswer/JpnLavender.scala
@@ -5,7 +5,8 @@ object Main extends App{
   var alfa = List("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
   var random = new Random()
   var parsedAlfa = Random.shuffle(alfa)
+  if (in == 26) in = 25
   for ( i <- 0 to in ) {
-    print(parsedAlfa(i))
+    print(parsedAlfa(i) + " ")
   }
 }
